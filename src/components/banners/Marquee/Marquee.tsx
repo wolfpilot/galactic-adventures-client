d09c70da3d695939bdd3f7167bdbfe1c92ba6e8e
Razvan Negrea <rznart@gmail.com>
2024-08-09 17:01:11 +0200

    Update Marquee to pause on SiteNav open

--- src/components/banners/Marquee/Marquee.tsx
+++ src/components/banners/Marquee/Marquee.tsx
@@ -1,18 +1,32 @@
 // Types
 import type { Props } from "./types"
 
+// Utils
+import { useBoundStore } from "@utils/stores/store"
+
 // Styles
 import styles from "./Marquee.module.css"
 
-const Marquee = ({ children, className = "" }: Props) => (
-  <div
-    className={`
+const Marquee = ({ children, className = "" }: Props) => {
+  const isNavOpen = useBoundStore((state) => state.isNavOpen)
+
+  return (
+    <div
+      className={`
         ${styles.wrapper}
         ${className}
       `}
-  >
-    <span className={styles.content}>{children}</span>
-  </div>
-)
+    >
+      <span
+        className={`
+          ${styles.content}
+          ${isNavOpen && styles.content__isPaused}
+        `}
+      >
+        {children}
+      </span>
+    </div>
+  )
+}
 
 export default Marquee
