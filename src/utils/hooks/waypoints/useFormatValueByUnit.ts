50a295cb8b3f97ab85dc8d4653bc2594656af440
Razvan Negrea <rznart@gmail.com>
2024-09-04 14:55:33 +0200

    Update formatter by unit to first parse results to locale string

--- src/utils/hooks/waypoints/useFormatValueByUnit.ts
+++ src/utils/hooks/waypoints/useFormatValueByUnit.ts
@@ -9,20 +9,25 @@ import {
   convertKelvinToCelsius,
 } from "@utils/helpers/number.helpers"
 
+// Utils
+const formatValue = (value: number) =>
+  value.toLocaleString(undefined, { maximumFractionDigits: 2 })
+
+// Exports
 export const useFormatDistanceUnit = (value: number | null) => {
   const unit = useBoundStore((state) => state.units.distance)
 
   if (isNullish(value)) return "N/A"
 
   if (unit === DistanceUnit.kilometres) {
-    return `${value}${DistanceUnit.kilometres}`
+    return `${formatValue(value)}${DistanceUnit.kilometres}`
   }
 
   const newVal = convertKilometresToMiles(value)
 
   if (isNaN(newVal)) return "N/A"
 
-  return `${newVal.toFixed(2)}${DistanceUnit.miles}`
+  return `${formatValue(newVal)}${DistanceUnit.miles}`
 }
 
 export const useFormatSpeedUnit = (value: number | null) => {
@@ -31,14 +36,14 @@ export const useFormatSpeedUnit = (value: number | null) => {
   if (isNullish(value)) return "N/A"
 
   if (unit === DistanceUnit.kilometres) {
-    return `${value}${SpeedUnit.kilometresPerHour}`
+    return `${formatValue(value)}${SpeedUnit.kilometresPerHour}`
   }
 
   const newVal = convertKilometresToMiles(value)
 
   if (isNaN(newVal)) return "N/A"
 
-  return `${newVal.toFixed(2)}${SpeedUnit.milesPerHour}`
+  return `${formatValue(newVal)}${SpeedUnit.milesPerHour}`
 }
 
 export const useFormatTemperatureUnit = (value: number | null) => {
@@ -47,14 +52,14 @@ export const useFormatTemperatureUnit = (value: number | null) => {
   if (isNullish(value)) return "N/A"
 
   if (unit === TemperatureUnit.kelvin) {
-    return `${value}${TemperatureUnit.kelvin}`
+    return `${formatValue(value)}${TemperatureUnit.kelvin}`
   }
 
   const newVal = convertKelvinToCelsius(value)
 
   if (isNaN(newVal)) return "N/A"
 
-  return `${newVal.toFixed(2)}${TemperatureUnit.celsius}`
+  return `${formatValue(newVal)}${TemperatureUnit.celsius}`
 }
 
 export const useFormatTemperatureToRange = (
