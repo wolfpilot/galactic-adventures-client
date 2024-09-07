fd61d723fa4ab51a0e044ffb89673936441a9b6f
Razvan Negrea <rznart@gmail.com>
2024-09-07 16:44:37 +0200

    Update the Error page to give a more detailed breakdown of the error

--- src/pages/error/ErrorPage.tsx
+++ src/pages/error/ErrorPage.tsx
@@ -1,3 +1,5 @@
+import { useRouteError, isRouteErrorResponse } from "react-router-dom"
+
 // Data
 import { errorPageData } from "./data/errorPage.data"
 
@@ -10,20 +12,51 @@ import { PageHeader, PageCta } from "@components/layout/Page"
 import Container from "@components/layout/Container/Container"
 import { ContentRow } from "@components/layout/Content"
 
-const ErrorPage = () => (
-  <>
-    <Head {...errorPageData.metadata} />
+// Utils
+const parseError = (error: unknown) => {
+  switch (true) {
+    case isRouteErrorResponse(error):
+      return {
+        title: error.status.toString(),
+        description: error.data?.message || error.statusText,
+      }
+    case error instanceof Error:
+      return {
+        title: "Oops!",
+        description: error.message,
+      }
+    case typeof error === "string":
+      return {
+        title: "Oops!",
+        description: error,
+      }
+    default:
+      console.error(error)
 
-    <PageHeader {...errorPageData.headerData} />
+      return errorPageData.defaultError
+  }
+}
 
-    <Container>
-      <ContentRow isPadded={false}>
-        <PageCta as="anchor" to={routes.home.url}>
-          Go back home
-        </PageCta>
-      </ContentRow>
-    </Container>
-  </>
-)
+const ErrorPage = () => {
+  const error = useRouteError()
+
+  const parsedError = parseError(error)
+
+  return (
+    <>
+      <Head {...parsedError} />
+
+      <PageHeader {...errorPageData.headerData} {...parsedError} />
+
+      <Container>
+        <ContentRow isPadded={false}>
+          <PageCta as="anchor" to={routes.home.url}>
+            Go back home?
+          </PageCta>
+        </ContentRow>
+      </Container>
+    </>
+  )
+}
 
 export default ErrorPage
--- src/pages/error/data/errorPage.data.ts
+++ src/pages/error/data/errorPage.data.ts
@@ -1,19 +1,16 @@
 // Types
-import { Props as MetadataProps } from "@components/layout/Head/types"
 import { Props as PageHeaderProps } from "@components/layout/Page/PageHeader/types"
 
 // Constants
 import { breakpoints } from "@constants/layout.constants"
 import { ERROR_PAGE_IMG } from "@constants/assets.constants"
 
-export const metadata: MetadataProps = {
+export const defaultError = {
   title: "Oops!",
   description: "Something went wrong...",
 }
 
-export const headerData: PageHeaderProps = {
-  title: "Oops!",
-  description: "Something went wrong...",
+export const headerData: Omit<PageHeaderProps, "title" | "description"> = {
   media: {
     type: "image",
     image: {
@@ -30,6 +27,6 @@ export const headerData: PageHeaderProps = {
 }
 
 export const errorPageData = {
-  metadata,
+  defaultError,
   headerData,
 }
